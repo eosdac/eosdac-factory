6e1d0a02c40f0d62b06c4c5f6152664d9d2c0ae1
Luke Stokes <luke.stokes@gmail.com>
2019-04-18 00:52:13 -0400

    Fix the eosio.cdt version to 1.5 as 1.6.x is giving me issues.

--- build_eos_tools.sh
+++ build_eos_tools.sh
@@ -12,16 +12,19 @@ if ! patch eosio_build.sh -R -p0 -s -f --dry-run -i ../eos.patch; then
 fi
 ./eosio_build.sh -s EOS
 cd ../eosio.cdt
-BUILD_VERSION="$(git tag --list | grep mainnet | sort | tail -1)"
+BUILD_VERSION="$(git tag --list | sort | tail -1)"
+echo "Latest version of eosio.cdt is $BUILD_VERSION but we're going to go ahead and use v1.5.0 as 1.6 has some bugs."
+BUILD_VERSION="v1.5.0"
+echo "Building $BUILD_VERSION..."
 git checkout $BUILD_VERSION
 git submodule update --init --recursive
 ./build.sh
-END_TIME="$(date)"
 echo "=================== eos and eosio.cdt built ================="
+END_TIME="$(date)"
 echo "Build Start Time: $START_TIME"
 echo "Build End Time: $END_TIME"
 
-read -p "Would you like to install eos and eosio.cdt? (Y/N)" response
+read -p "Would you like to install eos and eosio.cdt (requires sudo)? (Y/N)" response
 
 if [[ $response == "Y" || $response == "y" ]]; then
 	START_TIME="$(date)"
