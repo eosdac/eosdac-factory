d6488f14c81e1c7c852fa3373a64603370095f64
Luke Stokes <luke.stokes@gmail.com>
2019-04-18 00:55:55 -0400

    Updating the scripts to just run whatever is installed, not just the local folder.

--- jungle.sh
+++ jungle.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 cwd=$(pwd)
-$cwd/eos/build/bin/cleos --url http://jungle2.cryptolions.io:80/ "$@"
+# uncomment this next line if you want to run the specific version of cleos you built
+#$cwd/eos/build/bin/cleos --url http://jungle2.cryptolions.io:80/ "$@"
+cleos --url http://jungle2.cryptolions.io:80/ "$@"
 
--- keosd.sh
+++ keosd.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 cwd=$(pwd)
-$cwd/eos/build/bin/keosd --wallet-dir $cwd --http-server-address localhost:8900 "$@"
+# uncomment this next line if you want to run the specific version of keosd you built
+#$cwd/eos/build/bin/keosd --wallet-dir $cwd --http-server-address localhost:8900 "$@"
+keosd --wallet-dir $cwd --http-server-address localhost:8900 "$@"
